b7f75f9999cf3b104b48560b54c21de494a0ab31
Rene Garcia <rene@margar.fr>
2022-10-22 00:29:04 +0200

    New parameter NutOnBatteryDelay to shutdown system configurable seconds after running on battery

--- skeleton/opt/nut/bin/notify.sh
+++ skeleton/opt/nut/bin/notify.sh
@@ -2,7 +2,35 @@
 # Rene GARCIA (rene@margar.fr)
 # Script executed on ups event
 
+SHUDOWN_PID_FILE="/var/run/ups_shutdown.pid"
+
 . /opt/nut/etc/notify.conf
+
+# Delayed shutdown if running on battery
+if [ "${NOTIFYTYPE}" = "ONBATT" -a "${ONBATT_DELAY}" -gt 0 ]
+then
+  if [ ! -f "${SHUDOWN_PID_FILE}" ]
+  then
+    (
+      # seconds to wait
+      sleep "${ONBATT_DELAY}"
+      # force shutdown
+      rm "${SHUDOWN_PID_FILE}"
+      /opt/nut/sbin/upsd -c fsd
+      exit 0
+    ) &
+    echo $! > "${SHUDOWN_PID_FILE}"
+  fi
+fi
+
+# Abort delayed shutdown if UPS is back ONLINE
+if [ "${NOTIFYTYPE}" = "ONLINE" -a -f "${SHUDOWN_PID_FILE}" ]
+then
+  kill $(cat "${SHUDOWN_PID_FILE}")
+  rm "${SHUDOWN_PID_FILE}"
+fi
+
+# End here if no mail to send
 [ "${SEND_MAIL}" = 1 ] || exit 0
 
 DOMAIN="$(hostname -d)"
